3417fe7244dd7e5da781e3697bfd703c0a9b8e1c
Ashanur <ashanour009@gmail.com>
2020-09-29 01:40:57 +0600

    design menu section to service section

---
+++ js/active.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.slider-active').owlCarousel({
+        loop:true,
+        margin:10,
+        navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+});
